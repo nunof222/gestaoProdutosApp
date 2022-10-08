35cb389bfe010098e571170e344e4802facb75ea
nunof222 <19679544+nunof222@users.noreply.github.com>
2022-10-08 11:03:02 +0100

    Created Component Lista Produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaprodutosComponent } from './listaprodutos/listaprodutos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaprodutosComponent
   ],
   imports: [
     BrowserModule,
