071069f41b11d0380acac1110231ed42976bc7b1
nunof222 <19679544+nunof222@users.noreply.github.com>
2022-10-08 12:20:15 +0100

    added angular material library styles added create new product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewProductComponent } from './Components/create-new-product/create-new-product.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 const routes: Routes = [
 {path:'dashboard', component: DashboardComponent},
+{path:'newproduct', component: CreateNewProductComponent},
 {path:'', redirectTo:'/dashboard', pathMatch:'full'}             //set default page to Dashboard
 
 ];
