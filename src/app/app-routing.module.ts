7e2723e94165756a9429052567fa56442a714b5f
nunof222 <19679544+nunof222@users.noreply.github.com>
2022-10-08 12:06:21 +0100

    updated frontpage/dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'dashboard', component: DashboardComponent},
+{path:'', redirectTo:'/dashboard', pathMatch:'full'}             //set default page to Dashboard
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListaprodutosComponent } from './listaprodutos/listaprodutos.component';
+import { ListaprodutosComponent } from './Components/listaprodutos/listaprodutos.component';
+import { CreateNewProductComponent } from './Components/create-new-product/create-new-product.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaprodutosComponent
+    ListaprodutosComponent,
+    CreateNewProductComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
